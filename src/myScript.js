cc7703d57f47281448948cfe703542534db86b85
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 15:38:09 +0530

    Logical Order in If Else Statements

--- src/myScript.js
+++ src/myScript.js
@@ -408,3 +408,15 @@ function trueOrFalse(wasThatTrue) {
     }
   }
   testElseIf(7); //Subtask 72 ended
+
+  //Subtask 73 started
+  function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  orderMyLogic(7); //Subtask 73 ended
