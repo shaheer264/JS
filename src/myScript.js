63b7cf3560171e71ac52fffc4f5c8c6fcd4476ab
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 16:13:25 +0530

    Selecting from Many Options with Switch Statements

--- src/myScript.js
+++ src/myScript.js
@@ -471,3 +471,26 @@ function golfScore(par, strokes) {
   // Only change code above this line
 }
 golfScore(5, 4); //Subtask 75 ended
+
+//Subtask 76 started
+function caseInSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case 1:
+          answer = "alpha";
+          break;
+        case 2:
+          answer = "beta";
+          break;
+        case 3:
+          answer = "gamma";
+          break;
+        case 4:
+          answer = "delta";
+          break;
+    }
+    return answer;
+    // Only change code above this line
+  }
+  caseInSwitch(1); //Subtask 76 ended
