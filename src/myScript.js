014afd0a4fc365fcff643bf4fd6b7f40c52a82c1
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 15:33:21 +0530

    Introducing Else If Statements

--- src/myScript.js
+++ src/myScript.js
@@ -394,3 +394,17 @@ function trueOrFalse(wasThatTrue) {
     return result;
   }
   testElse(4); //Subtask 71 ended
+
+  //Subtask 72 started
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+    else {
+      return "Between 5 and 10";
+    }
+  }
+  testElseIf(7); //Subtask 72 ended
