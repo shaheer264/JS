e8d076eeb23b1e32d4310806990856ea42eb89c5
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 16:23:20 +0530

    Multiple Identical Options in Switch Statements

--- src/myScript.js
+++ src/myScript.js
@@ -517,3 +517,29 @@ function caseInSwitch(val) {
     return answer;
   }
   switchOfStuff(1); //Subtask 77 ended
+
+  //Subtask 78 started
+  function sequentialSizes(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+          case 1:
+          case 2:
+          case 3:
+            answer = "Low";
+            break;
+          case 4:
+          case 5:
+          case 6:
+            answer = "Mid";
+            break;
+          case 7:
+          case 8:
+          case 9:
+            answer = "High";
+            break;
+      }
+    // Only change code above this line
+    return answer;
+  }
+  sequentialSizes(1); //Subtask 78 ended
