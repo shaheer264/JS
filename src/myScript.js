ada997992c1d849024cf0125cf8c1eafc2461bc1
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 16:33:41 +0530

    Replacing If Else Chains with Switch

--- src/myScript.js
+++ src/myScript.js
@@ -543,3 +543,30 @@ function caseInSwitch(val) {
     return answer;
   }
   sequentialSizes(1); //Subtask 78 ended
+
+  //Subtask 79 started
+  function chainToSwitch(val) {
+    let answer = "";
+    // Only change code below this line
+    switch (val) {
+        case "bob":
+          answer = "Marley";
+          break;
+        case 42:
+          answer = "The Answer";
+          break;
+        case 1:
+          answer = "There is no #1";
+          break;
+        case 99:
+          answer = "Missed me by this much!";
+          break;
+        case 7:
+          answer = "Ate Nine";
+          break;
+    }
+  
+    // Only change code above this line
+    return answer;
+  }
+  chainToSwitch(7); //Subtask 79 ended
