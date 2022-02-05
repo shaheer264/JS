a846784a09b4d080d6a722b18e4be022af6813ac
shaheer@264 <shaheermustafahashmi@cedcoss.com>
2022-02-05 15:09:29 +0530

    Comparison with the Greater Than Operator

--- src/myScript.js
+++ src/myScript.js
@@ -299,3 +299,15 @@ function trueOrFalse(wasThatTrue) {
     return "Equal";
   }
   testStrictNotEqual(10); //Subtask 64 ended
+
+  //Subtask 65 started
+  function testGreaterThan(val) {
+    if (val > 100) {  // Change this line
+      return "Over 100";
+    }
+    if (val > 10) {  // Change this line
+      return "Over 10";
+    }
+    return "10 or Under";
+  }
+  testGreaterThan(10); //Subtask 65 ended
